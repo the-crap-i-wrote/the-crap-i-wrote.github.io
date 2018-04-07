e0cd65256824e7e9a5973c1a36f7a5a03562c53a
Wouter Saaltink <wouter.saaltink@gmail.com>
2018-04-08 01:49:07 +0200

    Update comments.js

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
   $('#comments').each(function(){
-      var commentsUrl = $(this).html();
-      var comments = $.ajax(commentsUrl);
-      $(this).html(comments);
+      var api_url = $(this).html();
+      $.ajax(api_url, {
+          headers: {Accept: "application/vnd.github.v3.html+json"},
+          dataType: "json",
+          success: function(comments) {
+              $("#comments").append("Visit the <b><a href='" + url + "'>Github Issue</a></b> to comment on this post");
+              $.each(comments, function(i, comment) {
+
+                  var date = new Date(comment.created_at);
+
+                  var t = "<div id='gh-comment'>";
+                  t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
+                  t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
+                  t += " posted at ";
+                  t += "<em>" + date.toUTCString() + "</em>";
+                  t += "<div id='gh-comment-hr'></div>";
+                  t += comment.body_html;
+                  t += "</div>";
+                  $("#gh-comments-list").append(t);
+              });
+          },
+          error: function() {
+              $("#gh-comments-list").append("Comments are not open for this post yet.");
+          }
+      });
     });
 });
